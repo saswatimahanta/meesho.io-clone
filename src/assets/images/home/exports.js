2a66f711afb69e75a134b0a406a4d1252e0afd98
Saswati1701 <saswatiisabella2002@gmail.com>
2024-07-29 11:53:48 +0530

    moved assets folder inside src, completed journey object

---
+++ src/assets/images/home/exports.js
@@ -0,0 +1,49 @@
+import pizza1 from "./pizza-1.png"
+import pizza2 from "./pizza-2.png"
+import pizza3 from "./pizza-3.png"
+import pizza4 from "./pizza-4.png"
+import pizza5 from "./pizza-5.png"
+import pizza6 from "./pizza-6.png"
+import pizza7 from "./pizza-7.png"
+import pizza8 from "./pizza-8.png"
+import pizza9 from "./pizza-9.png"
+import pizza10 from "./pizza-10.png"
+import pizza11 from "./pizza-11.png"
+import pizza12 from "./pizza-12.png"
+import pizza13 from "./pizza-13.png"
+import pizza14 from "./pizza-14.png"
+import pizza15 from "./pizza-15.png"
+import pizza16 from "./pizza-16.png"
+import pizza17 from "./pizza-17.png"
+import pizza18 from "./pizza-18.png"
+import pizza19 from "./pizza-19.png"
+import pizza20 from "./pizza-20.png"
+import pizza21 from "./pizza-21.png"
+import pizza22 from "./pizza-22.png"
+import pizza24 from "./pizza-24.png"
+
+export const pizza = [
+    pizza1, 
+    pizza2 ,
+    pizza3 ,
+    pizza4 ,
+    pizza5 ,
+    pizza6 ,
+    pizza7 ,
+    pizza8 ,
+    pizza9 ,
+    pizza10,
+    pizza11,
+    pizza12,
+    pizza13,
+    pizza14,
+    pizza15,
+    pizza16,
+    pizza17,
+    pizza18,
+    pizza19,
+    pizza20,
+    pizza21,
+    pizza22,
+    pizza24,
+]
--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,5 @@
-const home = {
+import {pizza} from "./assets/images/home/exports"
+export const home = {
     header:[
         "Building for a billion Indians",
         "Democratize Internet Commerce for Everyone"
@@ -18,90 +19,117 @@ const home = {
     ],
     journey:[
         {
+            image: pizza[10],
             date : "Aug 2015",
             text : 'We began with "Fashnear"- A hyperlocal fashion discovery platform',
         },
         {
+            image: pizza[9],
             date : "Mar 2016",
             text : "Pivoted to a mobile-first platform to help shop owners sell on WhatsApp & Facebook",
         },
         {
+            image: pizza[8],
             date:"Jun 2016",
             text: "Meesho is born, our founders get selected to Y Combinator"
         },
         {
+            image: pizza[7],
             date:"Feb 2019",
             text: "We hit 1.2 million monthly orders and ~200k transacting users",
         },
         {
+            image: pizza[6],
             date:"Jun 2019",
             text: "Facebook invests in us-first investment in an Indian startup",
         },
         {
+            image: pizza[5],
             date:"Feb 2020",
             text: "Our 1st ESOP buyback worth $1 million",
         },
         {
+            image: pizza[4],
             date:"Dec 2020",
             text: "Our 2nd ESOP buyback worth $5 million; 2nd within a span of a year",
         },
         {
+            image: pizza[3],
             date:"Jul 2021",
             text: "We become the first e-commerce platform to announce 0% commission for our sellers",
         },
         {
+            image: pizza[2],
             date:"Sep 2021",
             text: "We clock 51 million monthly orders & ~18 million transacting users. This is a 50x growth in 2 years",
         },
         {
+            image: pizza[1],
             date:"Sep 2021",
             text: "We’re the world’s most downloaded e-commerce mobile app",
         },
         {
+            image: pizza[0],
             date:"Nov 2021",
             text: "We’ve done 3 ESOP buybacks in ~2 years, helping create wealth for Meeshoites",
         },
         {
+            image: pizza[11],
             date:"April 2022",
             text: "Meesho Records Over 100 million Transacting Users",
         },
         {
+            image: pizza[12],
             date:"May 2022",
             text: "Hits 6 lakh sellers milestone",
         },
         {
+            image: pizza[13],
             date:"June 2022",
             text: "Records 53.5 lakh orders during a one-day sale event",
         },
         {
+            image: pizza[14],
             date:"Dec 2023",
             text: "Sees a record 91 crore orders placed in 2022",
         },
         {
+            image: pizza[15],
             date:"Jan 2023",
             text: "We crossed 10 crore+ listings on the platform",
         },
         {
+            image: pizza[16],
             date:"April 2023",
             text: "Meesho recorded 140 million annual transacting users on its platform – a sizable portion of India’s internet population",
         },
         {
+            image: pizza[17],
             date:"Sep 2022",
+            text: "Joins Open Network for Digital Commerce (ONDC) to help connect buyers with hyperlocal sellers",
+        },
+        {
+            image: pizza[18],
+            date:"Nov 2022",
             text: "Creates new record, clocks ~87.6 lakh orders on day one of Mega Blockbuster Sale",
         },
         {
+            image: pizza[19],
             date:"Dec 2022",
             text: "Nearly 130,000 sellers became lakhpatis and 6,000 sellers became crorepatis on Meesho through the year",
         },
         {
+            image: pizza[20],
             date:"Feb 2023",
             text: "Meesho Becomes India’s Fastest E-commerce Platform to Cross 10 lakh Sellers Milestone",
         },
         {
+            image: pizza[21],
             date:"Mar 2023",
             text: "Meesho became the fastest shopping app globally to cross 500 Mn downloads, as per data.ai",
         },
         {
+            image: pizza[22],
             date:"June 2023",
             text: "Meesho earned the coveted spot on TIME’s 2023 list of 100 most influential companies",
         },
