5e7dfb9163b213e1811f9e16911ebfb22e0cd0c3
Saswati1701 <saswatiisabella2002@gmail.com>
2024-07-29 10:29:11 +0530

    basic setup - create-react-app, assets containing all videos and images, constants.js containing all texts

---
+++ src/constants.js
@@ -0,0 +1,171 @@
+const home = {
+    header:[
+        "Building for a billion Indians",
+        "Democratize Internet Commerce for Everyone"
+    ],
+    subHeader:[
+        "OUR MISSION",
+        "HOW WE OPERATE",
+        "WE'RE GROWING",
+        "MARQUEE INVESTORS BELIEVE IN US",
+        "OUR JOURNEY IN BUILDING FOR A BILLION INDIANS",
+        "BUILD FOR BHARAT"
+    ],
+    description_text:[
+        "We take ZERO commissions from suppliers to list products and help anyone start a virtual store without any investments",
+        "We have opened up the world of online shopping to almost every household in India.",
+        
+    ],
+    journey:[
+        {
+            date : "Aug 2015",
+            text : 'We began with "Fashnear"- A hyperlocal fashion discovery platform',
+        },
+        {
+            date : "Mar 2016",
+            text : "Pivoted to a mobile-first platform to help shop owners sell on WhatsApp & Facebook",
+        },
+        {
+            date:"Jun 2016",
+            text: "Meesho is born, our founders get selected to Y Combinator"
+        },
+        {
+            date:"Feb 2019",
+            text: "We hit 1.2 million monthly orders and ~200k transacting users",
+        },
+        {
+            date:"Jun 2019",
+            text: "Facebook invests in us-first investment in an Indian startup",
+        },
+        {
+            date:"Feb 2020",
+            text: "Our 1st ESOP buyback worth $1 million",
+        },
+        {
+            date:"Dec 2020",
+            text: "Our 2nd ESOP buyback worth $5 million; 2nd within a span of a year",
+        },
+        {
+            date:"Jul 2021",
+            text: "We become the first e-commerce platform to announce 0% commission for our sellers",
+        },
+        {
+            date:"Sep 2021",
+            text: "We clock 51 million monthly orders & ~18 million transacting users. This is a 50x growth in 2 years",
+        },
+        {
+            date:"Sep 2021",
+            text: "We’re the world’s most downloaded e-commerce mobile app",
+        },
+        {
+            date:"Nov 2021",
+            text: "We’ve done 3 ESOP buybacks in ~2 years, helping create wealth for Meeshoites",
+        },
+        {
+            date:"April 2022",
+            text: "Meesho Records Over 100 million Transacting Users",
+        },
+        {
+            date:"May 2022",
+            text: "Hits 6 lakh sellers milestone",
+        },
+        {
+            date:"June 2022",
+            text: "Records 53.5 lakh orders during a one-day sale event",
+        },
+        {
+            date:"Dec 2023",
+            text: "Sees a record 91 crore orders placed in 2022",
+        },
+        {
+            date:"Jan 2023",
+            text: "We crossed 10 crore+ listings on the platform",
+        },
+        {
+            date:"April 2023",
+            text: "Meesho recorded 140 million annual transacting users on its platform – a sizable portion of India’s internet population",
+        },
+        {
+            date:"Sep 2022",
+            text: "Creates new record, clocks ~87.6 lakh orders on day one of Mega Blockbuster Sale",
+        },
+        {
+            date:"Dec 2022",
+            text: "Nearly 130,000 sellers became lakhpatis and 6,000 sellers became crorepatis on Meesho through the year",
+        },
+        {
+            date:"Feb 2023",
+            text: "Meesho Becomes India’s Fastest E-commerce Platform to Cross 10 lakh Sellers Milestone",
+        },
+        {
+            date:"Mar 2023",
+            text: "Meesho became the fastest shopping app globally to cross 500 Mn downloads, as per data.ai",
+        },
+        {
+            date:"June 2023",
+            text: "Meesho earned the coveted spot on TIME’s 2023 list of 100 most influential companies",
+        },
+    ],
+    transforming:{
+        transform:["Shaping", "Innovating", "Redefining", "Empowering"],
+        text:"e-commerce",
+    }
+}
+
+const tech = {
+    subHeader:[
+        "OUR SCALE",
+        "WHAT MAKES US DIFFERENT",
+        "THE MOVERS & SHAKERS",
+        "TECH STACK"
+    ],
+    transforming:{
+        transform:["Engineering", "Design & UR", "Product", "Data Science"],
+        text:"for Bharat"
+    }
+}
+
+const life = {
+    header:[
+        "Help us reimagine commerce for Bharat"
+    ],
+    subHeader:[
+        "OUR PROMISE TO YOU",
+        "VALUE WE SWEAR BY",
+        "WE GOT YOU",
+        "OUR LEADERSHIP",
+        "STORIES FROM OUR STABLES",
+        "POSH COMMITTEE"
+    ]
+}
+
+const blog = {
+    header:[
+        "Stories from the folks building for a billion Indians",
+        "Featured Posts",
+        "Product",
+        "Design & UR", 
+        "Engineering",
+        "Culture"
+    ],
+}
+
+const news = {
+    header:[
+        "Latest updates & developments from Meesho's newsroom", 
+        "Announcements",
+        "Social Impact",
+        "Triumphant Tales",
+        "Social Media",
+        "Brand Campaigns",
+        "Editorial Exchanges",
+        "Reports",
+        "Press Kit"
+    ],
+}
+
+const jobs = {
+    header:[
+        "Ready to take off on the Meesho Rocketship?"
+    ]
+}
